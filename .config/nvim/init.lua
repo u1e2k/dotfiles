c282fbd19fbdc543e136924a62adee8d66bda701
u1e2k <lied4u@gmail.com>
2025-06-07 21:09:09 +0900

    add config files

---
+++ .config/nvim/init.lua
@@ -0,0 +1,15 @@
+local opt = vim.opt
+
+-- 行番号の表示
+opt.number = true
+-- opt.relativenumber = true
+
+-- タブとインデントの設定
+opt.tabstop = 4
+opt.shiftwidth = 4
+opt.expandtab = true
+
+-- 検索設定
+opt.ignorecase = true
+opt.smartcase = true
+
