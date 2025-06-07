31be00c3f4174f39a20ed76759aed07ce72ff365
u1e2k <lied4u@gmail.com>
2025-06-07 18:07:43 +0900

    a

--- install.sh
+++ install.sh
@@ -23,21 +23,12 @@ for file in "${dotfiles[@]}"; do
         continue
     fi
 
-    # ホームディレクトリに同名のファイルまたはシンボリックリンクが既に存在する場合
-    if [ -e "$local_file" ] || [ -L "$local_file" ]; then
-        # 既存のファイルをバックアップ
-        backup_dir="$HOME/dotfiles_backup_$(date +%Y%m%d)"
-        mkdir -p "$backup_dir" # バックアップディレクトリが存在しなければ作成
-
-        echo "Backup: Moving existing '$local_file' to '$backup_dir/'"
-        mv "$local_file" "$backup_dir/$file"
-    fi
-
     # シンボリックリンクを張る
     echo "Creating symlink: '$source_file' -> '$local_file'"
-    ln -s "$source_file" "$local_file"
-    # -f オプションは不要になります（既存ファイルをmvで移動済みのため）
-    # -n オプションは通常不要ですが、もし必要なら追加しても良い
+    ln -sfbv "$source_file" "$local_file"
+    # -f force
+    # -b backup
+    # -n オプションは通常不要、もし必要なら追加しても良い
     # -v オプションは表示のため残しておく
 done
 
